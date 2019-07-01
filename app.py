dcf7d31f34279152f25e8bf86c1bbc3c3d246995
Ubuntu <ubuntu@ip-172-31-94-88.ec2.internal>
2019-07-01 10:02:04 +0000

    updated the task manager

--- app.py
+++ app.py
@@ -16,11 +16,18 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+    catagories=mongo.db.catagories.find())
     
-    
-
-
-
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
